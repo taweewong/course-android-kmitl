e45fc919b8282a1c54ba6a9f67a951edb79ea1d8
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-08-21 00:31:56 +0700

    send lab2 layout homework, this commit have only first exercise.

---
+++ lab02/Lab2Homework/app/src/main/java/com/homework/lab2/taweewong/lab2homework/MainActivity.java
@@ -0,0 +1,13 @@
+package com.homework.lab2.taweewong.lab2homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab2_homework_layout_1);
+    }
+}
