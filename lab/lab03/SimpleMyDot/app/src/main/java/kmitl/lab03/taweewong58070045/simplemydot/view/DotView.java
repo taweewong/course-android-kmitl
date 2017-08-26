3290afddc721a00d53273550fc42f3e86b8b98b8
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-08-26 17:40:20 +0700

    added property dots and addDot function in class DotView

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/view/DotView.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/view/DotView.java
@@ -7,39 +7,47 @@ import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
+import java.util.ArrayList;
 import kmitl.lab03.taweewong58070045.simplemydot.model.Dot;
 
 public class DotView extends View {
 
     private Paint paint;
     private Dot dot;
+    private ArrayList<Dot> dots;
 
     public DotView(Context context) {
         super(context);
         paint = new Paint();
+        dots = new ArrayList<>();
     }
 
     public DotView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint();
+        dots = new ArrayList<>();
     }
 
     public DotView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         paint = new Paint();
+        dots = new ArrayList<>();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         paint.setColor(Color.RED);
-        if (this.dot != null) {
+        if (!dots.isEmpty()) {
             canvas.drawCircle(dot.getCenterX(), dot.getCenterY(), 30, paint);
         }
     }
 
     public void setDot(Dot dot) {
         this.dot = dot;
+    }
+
+    public void addDot(Dot dot) {
+        dots.add(dot);
     }
 }
