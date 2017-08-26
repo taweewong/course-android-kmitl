06c38f87c18b877baa722dd70fb4c9d3e0739fde
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-08-26 18:54:57 +0700

    added function randomColor for each dot that created in class MainActivity

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/MainActivity.java
@@ -1,9 +1,9 @@
 package kmitl.lab03.taweewong58070045.simplemydot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import java.util.Random;
 
@@ -21,19 +21,27 @@ public class MainActivity extends AppCompatActivity implements Dot.DotChangedLis
         setContentView(R.layout.activity_main);
 
         dotView = (DotView) findViewById(R.id.dotView);
-        dot = new Dot(0, 0, 30, this);
     }
 
     public void onRandomDot(View view) {
         Random random = new Random();
         int centerX = random.nextInt(dotView.getWidth());
         int centerY = random.nextInt(dotView.getHeight());
-        new Dot(centerX, centerY, 30, this);
+        new Dot(centerX, centerY, 30, randomColor(), this);
     }
 
     @Override
     public void onDotChanged(Dot dot) {
         dotView.addDot(dot);
         dotView.invalidate();
+    }
+
+    private int randomColor() {
+        Random random = new Random();
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return Color.rgb(r, g, b);
     }
 }
