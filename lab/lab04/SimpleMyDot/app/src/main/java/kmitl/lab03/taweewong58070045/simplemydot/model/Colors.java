6b2e05e8c23a4a2f9dcc7335adea5de5d151f271
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-09 23:35:46 +0700

    added class Colors

---
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/model/Colors.java
@@ -0,0 +1,17 @@
+package kmitl.lab03.taweewong58070045.simplemydot.model;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class Colors {
+
+    public int randomColor() {
+        Random random = new Random();
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return Color.rgb(r, g, b);
+    }
+}
