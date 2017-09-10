ea837bb035699b5284d61a4e2b6049dd8dc7ff26
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-10 20:18:39 +0700

    moved MainActivity and SecondActivity to package controller

--- lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package kmitl.lab03.taweewong58070045.simplemydot;
+package kmitl.lab03.taweewong58070045.simplemydot.controller;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import kmitl.lab03.taweewong58070045.simplemydot.R;
 import kmitl.lab03.taweewong58070045.simplemydot.model.Colors;
 import kmitl.lab03.taweewong58070045.simplemydot.model.Dot;
 import kmitl.lab03.taweewong58070045.simplemydot.model.DotParcelable;
--- lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/SecondActivity.java
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/SecondActivity.java
@@ -1,4 +1,4 @@
-package kmitl.lab03.taweewong58070045.simplemydot;
+package kmitl.lab03.taweewong58070045.simplemydot.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import kmitl.lab03.taweewong58070045.simplemydot.R;
 import kmitl.lab03.taweewong58070045.simplemydot.model.DotParcelable;
 import kmitl.lab03.taweewong58070045.simplemydot.model.DotSerializable;
 
