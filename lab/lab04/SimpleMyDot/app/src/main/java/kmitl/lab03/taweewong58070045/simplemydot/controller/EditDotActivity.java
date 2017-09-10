befd98e4e40770f82a4d5f5c5dd82021df10eec5
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-10 21:36:59 +0700

    created class EditDotActivity

---
+++ lab/lab04/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/EditDotActivity.java
@@ -0,0 +1,15 @@
+package kmitl.lab03.taweewong58070045.simplemydot.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kmitl.lab03.taweewong58070045.simplemydot.R;
+
+public class EditDotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_dot);
+    }
+}
