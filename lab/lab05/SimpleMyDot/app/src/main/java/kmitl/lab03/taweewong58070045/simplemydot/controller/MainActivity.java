654584552825d90f72126fd476b187719d6e9da9
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-19 13:21:49 +0700

    deleted unused code in MainActivity

--- lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
@@ -1,36 +1,23 @@
 package kmitl.lab03.taweewong58070045.simplemydot.controller;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 import kmitl.lab03.taweewong58070045.simplemydot.R;
 import kmitl.lab03.taweewong58070045.simplemydot.fragment.DotViewFragment;
-import kmitl.lab03.taweewong58070045.simplemydot.model.Colors;
 import kmitl.lab03.taweewong58070045.simplemydot.model.Dot;
-import kmitl.lab03.taweewong58070045.simplemydot.model.Dots;
-import kmitl.lab03.taweewong58070045.simplemydot.utility.ScreenshotUtils;
-import kmitl.lab03.taweewong58070045.simplemydot.view.DotView;
 
-public class MainActivity extends AppCompatActivity implements Dots.OnDotsChangedListener, DotView.OnDotViewPressListener{
-
-    private DotView dotView;
-    private Dots dots;
+public class MainActivity extends AppCompatActivity {
 
     private final int EDIT_REQUEST = 1;
 
@@ -50,10 +37,6 @@ public class MainActivity extends AppCompatActivity implements Dots.OnDotsChange
     }
 
     private void initialize() {
-        dotView = (DotView) findViewById(R.id.dotView);
-        dotView.setListener(this);
-        dots = new Dots();
-        dots.setListener(this);
         initialFragment();
         setStrictMode();
     }
@@ -71,46 +54,19 @@ public class MainActivity extends AppCompatActivity implements Dots.OnDotsChange
                 .commit();
     }
 
-    public void onCaptureScreen() {
-        File file = ScreenshotUtils.store(ScreenshotUtils.getScreenshot(dotView),
-                "screenshot.jpg",
-                ScreenshotUtils.getMainDirectoryName(this));
-        shareImageFile(file);
+    private void updateFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
     }
 
-    public void onRandomDot(View view) {
-        Random random = new Random();
-        int centerX = random.nextInt(dotView.getWidth());
-        int centerY = random.nextInt(dotView.getHeight());
-        Dot newDot = new Dot(centerX, centerY, 30, Colors.randomColor());
-        dots.addDot(newDot);
-    }
-
-    public void onUndoDot(View view) {
-        dots.undo();
-    }
-
-    public void onClearDots(View view) {
-        dots.clearAll();
-    }
-
-    @Override
-    public void onDotsChanged(Dots dots) {
-        dotView.setDots(dots);
-        dotView.invalidate();
-    }
-
-    @Override
-    public void onDotViewPressed(int x, int y) {
-        int dotPosition = dots.findDot(x, y);
-
-        if (dotPosition == -1) {
-            Dot newDot = new Dot(x, y, 50, Colors.randomColor());
-            dots.addDot(newDot);
-        } else {
-            showAlertDialog(dotPosition);
-        }
-    }
+//    public void onCaptureScreen() {
+//        File file = ScreenshotUtils.store(ScreenshotUtils.getScreenshot(dotView),
+//                "screenshot.jpg",
+//                ScreenshotUtils.getMainDirectoryName(this));
+//        shareImageFile(file);
+//    }
 
     private void shareImageFile(File imageFile) {
         Intent intent = new Intent();
@@ -120,43 +76,11 @@ public class MainActivity extends AppCompatActivity implements Dots.OnDotsChange
         startActivity(Intent.createChooser(intent, "send image"));
     }
 
-    private void showAlertDialog(final int dotPosition) {
-        final List<String> optionList = new ArrayList<>();
-        optionList.add("Edit");
-        optionList.add("Delete");
-        CharSequence[] options = optionList.toArray(new String[optionList.size()]);
-
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setTitle("Edit or Delete ?");
-        dialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                switch (item) {
-                    case 0:
-                        startEditDotActivity(dots.getDot(dotPosition), dotPosition);
-                        break;
-                    case 1:
-                        dots.removeBy(dotPosition);
-                        Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        dialogBuilder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-
-        AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings:
-                onCaptureScreen();
+//                onCaptureScreen();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -171,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements Dots.OnDotsChange
                 int dotPosition = data.getIntExtra("dotPosition", -1);
 
                 if (dotPosition != -1) {
-                    dots.setDot(dotPosition, dot);
+//                    dots.setDot(dotPosition, dot);
                 }
             }
         }
