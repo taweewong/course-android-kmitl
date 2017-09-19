28c6d5b7d0bbbe61ebd61d957098370a2a11b394
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-19 13:14:45 +0700

    process dots and dotView in DotViewFragment instead of MainActivity

--- lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/fragment/DotViewFragment.java
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/fragment/DotViewFragment.java
@@ -1,26 +1,155 @@
 package kmitl.lab03.taweewong58070045.simplemydot.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import kmitl.lab03.taweewong58070045.simplemydot.R;
+import kmitl.lab03.taweewong58070045.simplemydot.model.Colors;
+import kmitl.lab03.taweewong58070045.simplemydot.model.Dot;
+import kmitl.lab03.taweewong58070045.simplemydot.model.Dots;
+import kmitl.lab03.taweewong58070045.simplemydot.view.DotView;
 
-public class DotViewFragment extends Fragment {
-
+public class DotViewFragment extends Fragment implements DotView.OnDotViewPressListener, Dots.OnDotsChangedListener, View.OnClickListener {
+    private Dots dots;
+    private DotView dotView;
 
     public DotViewFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        dots = new Dots();
+        dots.setListener(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dot_view, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dot_view, container, false);
+        initialView(rootView);
+        return rootView;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable("dots", dots);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            dots = savedInstanceState.getParcelable("dots");
+        }
+    }
+
+    private void initialView(View rootView) {
+        dotView = (DotView) rootView.findViewById(R.id.dotView);
+        Button randomDotButton = (Button) rootView.findViewById(R.id.randomDotButton);
+        Button clearDotButton = (Button) rootView.findViewById(R.id.clearDotButton);
+        Button undoButton = (Button) rootView.findViewById(R.id.undoButton);
+
+        dotView.setListener(this);
+        randomDotButton.setOnClickListener(this);
+        clearDotButton.setOnClickListener(this);
+        undoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDotViewPressed(int x, int y) {
+        int dotPosition = dots.findDot(x, y);
+
+        if (dotPosition == -1) {
+            Dot newDot = new Dot(x, y, 50, Colors.randomColor());
+            dots.addDot(newDot);
+        } else {
+            showAlertDialog(dotPosition);
+        }
+    }
+
+    @Override
+    public void onDotsChanged(Dots dots) {
+        dotView.setDots(dots);
+        dotView.invalidate();
+    }
+
+    private void showAlertDialog(final int dotPosition) {
+        final List<String> optionList = new ArrayList<>();
+        optionList.add("Edit");
+        optionList.add("Delete");
+        CharSequence[] options = optionList.toArray(new String[optionList.size()]);
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+        dialogBuilder.setTitle("Edit or Delete ?");
+        dialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                switch (item) {
+                    case 0:
+//                        startEditDotActivity(dots.getDot(dotPosition), dotPosition);
+                        Toast.makeText(getContext(), "Edit ja", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        dots.removeBy(dotPosition);
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.randomDotButton:
+                onRandomDot();
+                break;
+            case R.id.clearDotButton:
+                onClearDots();
+                break;
+            case R.id.undoButton:
+                onUndoDot();
+        }
+    }
+
+    public void onRandomDot() {
+        Random random = new Random();
+        int centerX = random.nextInt(dotView.getWidth());
+        int centerY = random.nextInt(dotView.getHeight());
+        Dot newDot = new Dot(centerX, centerY, 30, Colors.randomColor());
+        dots.addDot(newDot);
+    }
+
+    public void onUndoDot() {
+        dots.undo();
+    }
+
+    public void onClearDots() {
+        dots.clearAll();
+    }
 }
