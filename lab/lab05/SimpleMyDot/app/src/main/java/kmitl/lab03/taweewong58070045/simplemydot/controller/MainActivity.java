eba1749cfb98eb200ed4675a616f42438f4ad55d
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-09-22 17:06:48 +0700

    set animation when change fragment

--- lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab03/taweewong58070045/simplemydot/controller/MainActivity.java
@@ -41,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnDo
     private void updateFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_right
+                        , R.anim.slide_out_left
+                        , R.anim.slide_in_left
+                        , R.anim.slide_out_right)
                 .replace(R.id.fragmentContainer, fragment)
                 .addToBackStack(null)
                 .commit();
