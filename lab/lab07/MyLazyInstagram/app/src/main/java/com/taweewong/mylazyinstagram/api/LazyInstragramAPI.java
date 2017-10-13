1a5e94d73cc3202a43ab35b1069ea7e300c0458b
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-13 23:55:59 +0700

    renamed project name from LazyInstragram to LazyInstagram

---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/com/taweewong/mylazyinstagram/api/LazyInstragramAPI.java
@@ -0,0 +1,14 @@
+package com.taweewong.mylazyinstagram.api;
+
+import com.taweewong.mylazyinstagram.model.UserProfile;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface LazyInstragramAPI {
+    String BASE = "https://us-central1-retrofit-course.cloudfunctions.net";
+
+    @GET("/getProfile")
+    Call<UserProfile> getProfile(@Query("user") String user);
+}
---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/com/taweewong/mylazyinstagram/controller/MainActivity.java
@@ -0,0 +1,124 @@
+package com.taweewong.mylazyinstagram.controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.bumptech.glide.Glide;
+import com.taweewong.mylazyinstagram.R;
+import com.taweewong.mylazyinstagram.adapter.PostAdapter;
+import com.taweewong.mylazyinstagram.api.LazyInstragramAPI;
+import com.taweewong.mylazyinstagram.model.Post;
+import com.taweewong.mylazyinstagram.model.UserProfile;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity{
+    RecyclerView recyclerView;
+    ToggleButton linearViewButton;
+    ToggleButton gridViewButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getUserProfile("cartoon");
+
+        linearViewButton = findViewById(R.id.linearViewButton);
+        gridViewButton = findViewById(R.id.gridViewButton);
+
+        linearViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (linearViewButton.isChecked()) {
+                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    disable(linearViewButton);
+                    toggle(gridViewButton);
+                }
+            }
+        });
+
+        gridViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (gridViewButton.isChecked()) {
+                    recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+                    disable(gridViewButton);
+                    toggle(linearViewButton);
+                }
+            }
+        });
+    }
+
+    private void toggle(ToggleButton toggleButton) {
+        toggleButton.setEnabled(true);
+        toggleButton.setChecked(!toggleButton.isChecked());
+    }
+
+    private void disable(ToggleButton toggleButton) {
+        toggleButton.setEnabled(false);
+    }
+
+    private void getUserProfile(final String userName) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(client)
+                .baseUrl(LazyInstragramAPI.BASE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        LazyInstragramAPI lazyInstragramAPI = retrofit.create(LazyInstragramAPI.class);
+
+        Call<UserProfile> call = lazyInstragramAPI.getProfile(userName);
+        call.enqueue(new Callback<UserProfile>() {
+            @Override
+            public void onResponse(Call<UserProfile> call, Response<UserProfile> response) {
+                if (response.isSuccessful()) {
+                    UserProfile userProfile = response.body();
+
+                    setMainActivityDisplay(userProfile);
+                    setMainActivityAdapter(userProfile.getPosts());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserProfile> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void setMainActivityAdapter(Post[] posts) {
+        PostAdapter postAdapter = new PostAdapter(MainActivity.this, posts);
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+        recyclerView.setAdapter(postAdapter);
+    }
+
+    private void setMainActivityDisplay(UserProfile userProfile) {
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        ImageView userImage = findViewById(R.id.userImage);
+        TextView postTextView = findViewById(R.id.postTextView);
+        TextView followerTextView = findViewById(R.id.followerTextView);
+        TextView followingTextView = findViewById(R.id.followingTextView);
+        TextView bioTextView = findViewById(R.id.bioTextView);
+
+        usernameTextView.setText(userProfile.getUser());
+        Glide.with(MainActivity.this).load(userProfile.getUrlProfile()).into(userImage);
+        postTextView.setText(String.valueOf(userProfile.getPost()));
+        followerTextView.setText(String.valueOf(userProfile.getFollower()));
+        followingTextView.setText(String.valueOf(userProfile.getFollowing()));
+        bioTextView.setText(userProfile.getBio());
+    }
+}
