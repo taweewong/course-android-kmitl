4f1af2ff94a38eb554738b2bd011f1af1515a335
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-18 11:51:33 +0700

    refactored class ProgressIndicator

--- lab/lab07/MyLazyInstagram/app/src/main/java/com/taweewong/mylazyinstagram/utility/ProgressIndicator.java
+++ lab/lab07/MyLazyInstagram/app/src/main/java/com/taweewong/mylazyinstagram/utility/ProgressIndicator.java
@@ -10,24 +10,23 @@ import android.view.Window;
 import com.taweewong.mylazyinstagram.R;
 
 public class ProgressIndicator {
-    private Context context;
     private Dialog progressDialog;
 
     public ProgressIndicator(Context context) {
-        this.context = context;
-    }
-
-    public void show() {
-        progressDialog = new Dialog(context);
         View view = LayoutInflater.from(context).inflate(R.layout.loading, null);
+
+        progressDialog = new Dialog(context);
         progressDialog.setCancelable(false);
         progressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         progressDialog.setContentView(view);
+    }
+
+    public void show() {
         progressDialog.show();
     }
 
     public void hide() {
-        progressDialog.dismiss();
+        progressDialog.hide();
     }
 }
