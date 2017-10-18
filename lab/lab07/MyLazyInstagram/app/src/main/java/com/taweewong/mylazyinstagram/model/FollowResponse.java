e42bc32251425a201d3eb9382fac85a9bc5b18c9
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-18 11:47:46 +0700

    created class FollowResponse

---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/com/taweewong/mylazyinstagram/model/FollowResponse.java
@@ -0,0 +1,13 @@
+package com.taweewong.mylazyinstagram.model;
+
+public class FollowResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
