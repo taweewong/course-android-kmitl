fb2cbf885a66c65226f9398d87e0e146dd471def
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-25 00:26:01 +0700

    added method ListIndexZeroTest and childAtPosition in class MainActivityTest

--- lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
+++ lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
@@ -4,7 +4,13 @@ package com.project.demorecord;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +22,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -52,5 +59,33 @@ public class MainActivityTest {
         onView(withId(R.id.editTExtName)).perform(replaceText("YING"));
         onView(withId(R.id.buttonAdded)).perform(click());
         onView(withText("Please Enter user info")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ListIndexZeroTest() {
+        onView(withId(R.id.editTExtName)).perform(replaceText("YING"));
+        onView(withId(R.id.editTextAge)).perform(replaceText("20"));
+        onView(withId(R.id.buttonAdded)).perform(click());
+        onView(withId(R.id.buttonGotoList)).perform(click());
+        onView(allOf((childAtPosition(withId(R.id.list), 0)), withText("YING"), withText("20"), isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
