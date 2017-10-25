c55dca7e70cc030d0ab82198044db5a3dfe37424
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-25 12:00:57 +0700

    added method listIndexOneTest in class MainActivityTest

--- lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
+++ lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
@@ -74,6 +74,17 @@ public class MainActivityTest {
         onView(withRecyclerView(R.id.list, 0)).check(matches(hasDescendant(withText("20"))));
     }
 
+    @Test
+    public void listIndexOneTest() {
+        onView(withId(R.id.editTExtName)).perform(typeText("Ladarat"));
+        onView(withId(R.id.editTextAge)).perform(typeText("20"));
+        pressBack();
+        onView(withId(R.id.buttonAdded)).perform(click());
+        onView(withId(R.id.buttonGotoList)).perform(click());
+        onView(withRecyclerView(R.id.list, 1)).check(matches(hasDescendant(withText("Ladarat"))));
+        onView(withRecyclerView(R.id.list, 1)).check(matches(hasDescendant(withText("20"))));
+    }
+
     private Matcher<View> withRecyclerView(int id, int index) {
         return childAtPosition(childAtPosition(withId(id), index), 0);
     }
