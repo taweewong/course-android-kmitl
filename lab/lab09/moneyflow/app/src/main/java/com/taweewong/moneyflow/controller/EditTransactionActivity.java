92699c8467320c877ee8150c3a40b3cc42b00e41
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-04 23:05:47 +0700

    created class EditTransactionActivity

---
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/EditTransactionActivity.java
@@ -0,0 +1,15 @@
+package com.taweewong.moneyflow.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.taweewong.moneyflow.R;
+
+public class EditTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_transaction);
+    }
+}
