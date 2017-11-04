ed63456aca3bac64d889b6897f53c2eaa9a598ea
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-04 23:01:37 +0700

    set onclick listener to addTransactionText in class MainActivity

--- lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
@@ -1,16 +1,27 @@
 package com.taweewong.moneyflow.controller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.taweewong.moneyflow.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0);
+
+        TextView addTransactionText = findViewById(R.id.addTransactionText);
+        addTransactionText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, AddTransactionActivity.class));
     }
 }
