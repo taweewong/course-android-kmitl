e16fa04e23e1709415df3f077044a597af74744d
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-04 20:25:24 +0700

    moved class MainActivity to package controller

--- lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.taweewong.moneyflow;
+package com.taweewong.moneyflow.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.taweewong.moneyflow.R;
 
 public class MainActivity extends AppCompatActivity {
 
