400cdefede09012ea887d8c3663bbd734aa66c45
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-04 22:51:50 +0700

    edited AddTransactionActivity's layout

--- lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/AddTransactionActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/AddTransactionActivity.java
@@ -1,7 +1,8 @@
 package com.taweewong.moneyflow.controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.taweewong.moneyflow.R;
 
@@ -11,5 +12,18 @@ public class AddTransactionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+        getSupportActionBar().setElevation(0);
+        setTitle("Add Transaction");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+        return true;
+    }
+
 }
