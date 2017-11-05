7b07e4349a9fe1abfe91f99aea3373cd0ae47c21
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-05 21:30:57 +0700

    added entity Transaction

---
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/entity/Transaction.java
@@ -0,0 +1,71 @@
+package com.taweewong.moneyflow.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "TRANSACTION")
+public class Transaction {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "amount")
+    private float amount;
+
+    @ColumnInfo(name = "note")
+    private String note;
+
+    @ColumnInfo(name = "date")
+    private String date;
+
+    @ColumnInfo(name = "type")
+    private String type;
+
+    public Transaction(float amount, String note, String date, String type) {
+        this.amount = amount;
+        this.note = note;
+        this.date = date;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
