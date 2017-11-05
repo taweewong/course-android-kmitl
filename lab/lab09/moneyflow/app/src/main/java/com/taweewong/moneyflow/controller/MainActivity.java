640dda2fdf50b1248a0b4c8494225f188ec7fe29
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-05 21:33:15 +0700

    connected database with RxJava

--- lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/MainActivity.java
@@ -1,12 +1,23 @@
 package com.taweewong.moneyflow.controller;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.taweewong.moneyflow.R;
+import com.taweewong.moneyflow.dao.TransactionDAO;
+import com.taweewong.moneyflow.database.TransactionDB;
+import com.taweewong.moneyflow.entity.Transaction;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +29,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView addTransactionText = findViewById(R.id.addTransactionText);
         addTransactionText.setOnClickListener(this);
+
+        TransactionDB transactionDB = Room.databaseBuilder(this,
+                TransactionDB.class, "Transaction")
+                .fallbackToDestructiveMigration()
+                .build();
+
+        Transaction transaction = new Transaction(150, "note", "date", "type");
+
+        TransactionDAO dao = transactionDB.getTransactionDAO();
+
+        Observable.fromCallable(() -> dao.insertTransaction(transaction))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d("Observe", "subscribe");
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.d("Observe", "next");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d("Observe", "error");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d("Observe", "complete");
+
+                    }
+                });
+
+        Observable.fromCallable(dao::getAllTransaction)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(transactions -> {
+                   for (Transaction trans : transactions) {
+                       Log.d("Observe", trans.getId() + " : " + trans.getAmount());
+                   }
+                });
+
     }
 
     @Override
