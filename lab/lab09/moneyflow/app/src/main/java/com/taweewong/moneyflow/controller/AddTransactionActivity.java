97f035de342e5e04bf13960e3b1b11be278f762f
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-07 02:07:21 +0700

    set onClick to button in AddTransactionActivity

--- lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/AddTransactionActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/com/taweewong/moneyflow/controller/AddTransactionActivity.java
@@ -3,10 +3,16 @@ package com.taweewong.moneyflow.controller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 import com.taweewong.moneyflow.R;
 
-public class AddTransactionActivity extends AppCompatActivity {
+public class AddTransactionActivity extends AppCompatActivity implements View.OnClickListener {
+    ToggleButton addIncomeTypeButton;
+    ToggleButton addExpenseTypeButton;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,14 @@ public class AddTransactionActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(0);
         setTitle("Add Transaction");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addIncomeTypeButton = findViewById(R.id.addIncomeTypeButton);
+        addExpenseTypeButton = findViewById(R.id.addExpenseTypeButton);
+        addButton = findViewById(R.id.addButton);
+
+        addIncomeTypeButton.setOnClickListener(this);
+        addExpenseTypeButton.setOnClickListener(this);
+        addButton.setOnClickListener(this);
     }
 
     @Override
@@ -26,4 +40,23 @@ public class AddTransactionActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addIncomeTypeButton:
+                toggleTwoButton(addIncomeTypeButton, addExpenseTypeButton);
+                break;
+            case R.id.addExpenseTypeButton:
+                toggleTwoButton(addExpenseTypeButton, addIncomeTypeButton);
+                break;
+            case R.id.addButton:
+        }
+    }
+
+    private void toggleTwoButton(ToggleButton toCheckedButton, ToggleButton toUncheckedButton) {
+        toCheckedButton.setEnabled(false);
+        toCheckedButton.setChecked(true);
+        toUncheckedButton.setEnabled(true);
+        toUncheckedButton.setChecked(false);
+    }
 }
